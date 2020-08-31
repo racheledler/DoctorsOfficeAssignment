5caa9e2be72329bfa3d7a8ea3f805fd3a942ca05
racheledler <55890230+racheledler@users.noreply.github.com>
2020-08-31 17:05:39 -0400

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,23 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String patientName = "Stacey Manson";
+        boolean hasInsurance = false;
+        float temp = 102.2f;
+        int numVisits = 5;
+        float copay = 75.00f;
+        String nextappt = "December 20, 2020";
+        String contactnum = "678-899-3720";
+
+        System.out.println("Patient Details:");
+        System.out.println("Name: " + patientName);
+        System.out.println("Patient Has Insurance: " + hasInsurance);
+        System.out.println("Body Temperature: " + temp);
+        System.out.println("Number of Visits: " + numVisits);
+        System.out.println("Patient Copay Amount: " + copay);
+        System.out.println("Next Appointment: " + nextappt);
+        System.out.println("Contact Number: " + contactnum); }
+    }
